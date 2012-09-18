b5cd6f99883947b1540404a69523945ccc02fe1a
jewoo <whwpdn@nate.com>
2012-09-18 13:29:47 +0900

    TEST
    TEST 주석

--- src/com/smarteco/android/C.java
+++ src/com/smarteco/android/C.java
@@ -7,5 +7,5 @@ public class C {
 	public static final String FACEBOOK_APP_ID="409336535779325";
 	public static final int FACEBOOK_AUTH_CODE = 32665;
 	public static final CharSequence[] items = {"수정","삭제"};
-	
+	// JUSUCK	
 }
